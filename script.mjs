890744a43a78e588283f63ce8dd527a2127c221e
Brett Gurzick <59893116+bgurzick@users.noreply.github.com>
2024-05-15 13:49:51 -0700

    second commit - not quite done

--- script.mjs
+++ script.mjs
@@ -1,1 +1,27 @@
-import "./styles.css";
+const mainEl = document.getElementsByTagName('main');
+console.log(mainEl[0])
+
+mainEl[0].style.backgroundColor = 'var(--main-bg)';
+mainEl[0].innerHTML = `<h1>DOM Manipulation</h1>`;
+mainEl[0].classList.add(`flex-ctr`);
+
+const topMenuEl = document.getElementById(`top-menu`);
+console.log(topMenuEl)
+
+topMenuEl.style.height = `100%`;
+
+topMenuEl.style.backgroundColor =`var(--top-menu-bg)`;
+
+topMenuEl.classList.add('flex-around');
+
+
+menuLinks.forEach((link)=>{
+    let newLink=document.createElement('a')
+
+    newLink.setAttribute('href', link.href)
+
+    newLink.textContent = link.text
+
+    topMenuEl.appendChild(newLink)
+})
+
